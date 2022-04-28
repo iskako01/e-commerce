cbefcc76951be4b9e02e63ecfa44bea0713dab1a
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-04-28 23:07:30 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout
+    view="lHh lpr lFf"
+    container
+    style="height: 500px"
+    class="shadow-2 rounded-borders wrapper"
+  >
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style scoped>
+img {
+  width: 100%;
+}
+</style>
